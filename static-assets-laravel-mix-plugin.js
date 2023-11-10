dd9ecd13088138552463d199bc9f2309360d9cb9
Benjamin Ayles <ben@ayles.com.au>
2023-11-10 15:58:36 +1030

    wip

---
+++ static-assets-laravel-mix-plugin.js
@@ -0,0 +1,10 @@
+let mix = require('laravel-mix');
+
+if (process.env.STATIC_ASSETS_BUILD) {
+    mix.setPublicPath('public/build')
+        .options({
+            processCssUrls: true,
+        })
+        .setResourceRoot(process.env.STATIC_ASSETS_URL);
+}
+
